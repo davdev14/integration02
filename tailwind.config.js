c9f7e4a5146ba5a7d8f4640460803f20984efd97
davdev14 <davdev1400@gmail.com>
2023-07-19 11:47:35 +0100

    working on navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     "./src/**/*.{html,js,ts}"
   ],
   theme: {
-    extend: {},
+    screens: {
+      "2xl": "1440px"
+    },
+    extend: {
+      fontFamily: {
+        mintaka: ["mintaka", "sans"],
+        causten: ["causten", "sans"],
+        core: ["core", "sans"]
+      },
+      colors: {
+        'euphoria-black': '#3C4242',
+        'euphoria-grey': '#807D7E',
+        'euphoria-white-bg': '#F6F6F6',
+      }
+    },
   },
   plugins: [],
 }
